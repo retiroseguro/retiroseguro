e3e1e52624b1f90403e87aafce4b90940a73936c
ortega <ortega.alejandra.molina@gmail.com>
2022-07-07 21:41:29 -0400

    botones

--- src/components/Botones.vue
+++ src/components/Botones.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button>
+    <button type="button" class="btn btn-fondo btn-lg">
         ENVIAR
     </button>
   </div>
@@ -12,6 +12,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../assets/scss/colores.scss";
+@import "../assets/scss/tipografia.scss";
 
+.btn-fondo {
+    background-color: $secundario;
+}
+.btn {
+    color: $blanco;
+    font-family: $inter;
+}
+.btn:hover{
+  color: $blanco !important;
+}
 </style>
